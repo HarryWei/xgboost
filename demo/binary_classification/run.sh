661837a873ad668b3e85c28848bdbd2e98f36dbc
Xiaowei Shang <xiaowei.shang16@gmail.com>
2018-04-12 07:33:39 -0400

    add valgrind data in vms

--- demo/binary_classification/run.sh
+++ demo/binary_classification/run.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 
-../../xgboost mushroom.conf nthread=$1 &
-
-sleep 5
-
-process_id=`/bin/ps -aux | grep "xgboost" | grep -v "grep" | grep -v "bash" | awk '{print $2}'`
-
-#echo $process_id
-
-cd /proc/$process_id/task
-
-i=0
-for entry in *
-do
-	/usr/bin/taskset -pc $i $entry
-	((i=$i+1))
-done
+valgrind --tool=callgrind ../../xgboost mushroom.conf nthread=1
+valgrind --tool=callgrind ../../xgboost mushroom.conf nthread=16
